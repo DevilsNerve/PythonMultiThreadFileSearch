a40400ee1ac043ffe02bdf235d845f37d1f9713d
DevilsNerve <austengreenj@gmail.com>
2023-07-25 12:34:02 -0500

    Update Search.py
    Add queue and file copy path

--- Search.py
+++ Search.py
@@ -6,14 +6,15 @@ from tkinter import filedialog, messagebox
 from mmap import mmap, ACCESS_READ
 import concurrent.futures
 import multiprocessing
+import re
 
 
 # Function to search a single file
 def search_file(filename, search_text, file_extension):
     matches = []
     _, ext = os.path.splitext(filename)
-    if file_extension == "*.*" or ext == file_extension:
-        if search_text in filename:
+    if file_extension == "*.*" or re.fullmatch(file_extension, ext):
+        if search_text.lower() in filename.lower():
             matches.append(filename)
         else:
             with open(filename, 'r', errors='ignore') as f:
@@ -24,6 +25,28 @@ def search_file(filename, search_text, file_extension):
                 except ValueError:
                     pass
     return filename, matches
+
+
+class SearchQueue:
+    def __init__(self):
+        self.queue = []
+        self.current_search = None
+
+    def add_search(self, directory, search_text, file_extension):
+        self.queue.append((directory, search_text, file_extension))
+
+    def next_search(self):
+        if self.queue:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def is_searching(self):
+        return self.current_search is not None and self.current_search.is_alive()
+
+    def cancel_current_search(self):
+        if self.is_searching():
+            self.current_search.cancel_search()
 
 
 class FileSearcherThread(threading.Thread):
@@ -38,11 +61,14 @@ class FileSearcherThread(threading.Thread):
                 self.files.append(os.path.join(foldername, filename))
         self.results = []
         self.searched = []
+        self.cancelled = False
 
     def run(self):
         with concurrent.futures.ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
             future_to_file = {executor.submit(search_file, file, self.search_text, self.file_extension): file for file in self.files}
             for future in concurrent.futures.as_completed(future_to_file):
+                if self.cancelled:
+                    return
                 file = future_to_file[future]
                 try:
                     searched_file, matches = future.result()
@@ -51,19 +77,8 @@ class FileSearcherThread(threading.Thread):
                 except Exception as exc:
                     print(f'File {file} generated an exception: {exc}')
 
-
-class ResultFrame(tk.Frame):
-    def __init__(self, parent, result):
-        tk.Frame.__init__(self, parent)
-        self.result = result
-        self.label = tk.Label(self, text=result)
-        self.label.pack(side=tk.LEFT, fill=tk.X, expand=True)
-        self.button = tk.Button(self, text="Copy", command=self.copy)
-        self.button.pack(side=tk.RIGHT)
-
-    def copy(self):
-        self.clipboard_clear()
-        self.clipboard_append(self.result)
+    def cancel_search(self):
+        self.cancelled = True
 
 
 class App:
@@ -71,58 +86,66 @@ class App:
         self.root = root
         self.root.title("Austen's Fast File Search")
 
-        # Configure grid weights
-        self.root.columnconfigure(0, weight=1)
-        for i in range(6):
-            self.root.rowconfigure(i, weight=1 if i==4 else 0)
-
         self.label_search = tk.Label(self.root, text="Enter search text:")
-        self.label_search.grid(row=0, column=0, sticky='ew')
+        self.label_search.pack(fill='x')
         self.search_text = tk.StringVar()
         self.entry_search = tk.Entry(self.root, textvariable=self.search_text)
-        self.entry_search.grid(row=1, column=0, sticky='ew')
+        self.entry_search.pack(fill='x')
 
         self.label_extension = tk.Label(self.root, text="Enter file extension:")
-        self.label_extension.grid(row=2, column=0, sticky='ew')
+        self.label_extension.pack(fill='x')
         self.file_extension = tk.StringVar(value="*.*")
         self.entry_extension = tk.Entry(self.root, textvariable=self.file_extension)
-        self.entry_extension.grid(row=3, column=0, sticky='ew')
+        self.entry_extension.pack(fill='x')
 
         self.button = tk.Button(self.root, text="Search", command=self.search)
-        self.button.grid(row=4, column=0, sticky='ew')
+        self.button.pack(fill='x')
 
         self.progress = ttk.Progressbar(self.root)
-        self.progress.grid(row=5, column=0, sticky='ew')
+        self.progress.pack(fill='x')
 
         self.label_results = tk.Label(self.root, text="Search results:")
-        self.label_results.grid(row=6, column=0, sticky='ew')
+        self.label_results.pack(fill='x')
 
-        self.results_frame = tk.Frame(self.root)
-        self.results_frame.grid(row=7, column=0, sticky='nsew')
-
-        self.scrollbar = tk.Scrollbar(self.results_frame)
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-        self.results = tk.Listbox(self.results_frame, yscrollcommand=self.scrollbar.set)
-        self.results.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        
-        self.scrollbar.config(command=self.results.yview)
+        self.results = tk.Listbox(self.root)
+        self.results.pack(fill='both', expand=True)
+        self.results.bind('<<ListboxSelect>>', self.copy_to_clipboard)
 
         self.label_searched = tk.Label(self.root, text="Searched files:")
-        self.label_searched.grid(row=8, column=0, sticky='ew')
+        self.label_searched.pack(fill='x')
 
         self.searched = tk.Listbox(self.root)
-        self.searched.grid(row=9, column=0, sticky='nsew')
+        self.searched.pack(fill='both', expand=True)
+
+        self.label_queue = tk.Label(self.root, text="Search queue:")
+        self.label_queue.pack(fill='x')
+
+        self.queue = tk.Listbox(self.root)
+        self.queue.pack(fill='both', expand=True)
+
+        self.queue_up_button = tk.Button(self.root, text="Move Up in Queue", command=self.move_up_in_queue)
+        self.queue_up_button.pack(fill='x')
+
+        self.queue_down_button = tk.Button(self.root, text="Move Down in Queue", command=self.move_down_in_queue)
+        self.queue_down_button.pack(fill='x')
+
+        self.queue_remove_button = tk.Button(self.root, text="Remove from Queue", command=self.remove_from_queue)
+        self.queue_remove_button.pack(fill='x')
+
+        self.search_queue = SearchQueue()
 
     def search(self):
-        directory = filedialog.askdirectory()
-        search_text = self.search_text.get()
-        file_extension = self.file_extension.get()
-        self.results.delete(0, tk.END)
-        self.searched.delete(0, tk.END)
-        searcher = FileSearcherThread(directory, search_text, file_extension)
-        searcher.start()
-        self.root.after(100, self.check_thread, searcher)
+        if self.search_queue.is_searching():
+            self.search_queue.cancel_current_search()
+            self.button.configure(text="Search")
+        else:
+            directory = filedialog.askdirectory()
+            search_text = self.search_text.get()
+            file_extension = self.file_extension.get()
+            self.search_queue.add_search(directory, search_text, file_extension)
+            self.queue.insert(tk.END, f"{search_text} in {directory} ({file_extension})")
+            self.check_queue()
+            self.button.configure(text="Cancel")
 
     def check_thread(self, thread):
         self.progress['value'] = len(thread.searched)
@@ -134,7 +157,55 @@ class App:
         else:
             for result in thread.results:
                 self.results.insert(tk.END, result)
-            messagebox.showinfo("Search Complete", "The file search has been completed.")
+            self.queue.delete(0)
+            self.check_queue()
+
+    def check_queue(self):
+        if not self.search_queue.is_searching():
+            next_search = self.search_queue.next_search()
+            if next_search:
+                directory, search_text, file_extension = next_search
+                searcher = FileSearcherThread(directory, search_text, file_extension)
+                self.search_queue.current_search = searcher
+                searcher.start()
+                self.root.after(100, self.check_thread, searcher)
+            else:
+                self.button.configure(text="Search")
+                messagebox.showinfo("Search Complete", "The file search has been completed.")
+
+    def move_up_in_queue(self):
+        selected = self.queue.curselection()
+        if selected:
+            for index in selected:
+                if index != 0:
+                    value = self.queue.get(index)
+                    self.queue.delete(index)
+                    self.queue.insert(index - 1, value)
+                    self.queue.selection_set(index - 1)
+
+    def move_down_in_queue(self):
+        selected = self.queue.curselection()
+        if selected:
+            for index in reversed(selected):
+                if index != self.queue.size() - 1:
+                    value = self.queue.get(index)
+                    self.queue.delete(index)
+                    self.queue.insert(index + 1, value)
+                    self.queue.selection_set(index + 1)
+
+    def remove_from_queue(self):
+        selected = self.queue.curselection()
+        if selected:
+            for index in reversed(selected):
+                self.queue.delete(index)
+
+    def copy_to_clipboard(self, event):
+        self.root.clipboard_clear()
+        selection = event.widget.curselection()
+        if selection:
+            selected = event.widget.get(selection)
+            self.root.clipboard_append(selected)
+
 
 root = tk.Tk()
 app = App(root)
